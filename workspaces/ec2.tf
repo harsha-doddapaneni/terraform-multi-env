cf7e74c9bfd3caef84f01de5658feb2f7fbb9332
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-04-11 10:17:02 +0530

    workspaces

---
+++ workspaces/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "terraform" {
+    #count = terraform.workspace == "prod" ? 2 : 1
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = lookup(var.instance_type, terraform.workspace)
+    vpc_security_group_ids = ["sg-0c41fb615bc42375e"]
+    tags = {
+        Name = "terraform-${terraform.workspace}"
+    }
+}
