fc2e4570132b3983a6eb53d928d7ada52213ec68
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-04-11 10:11:25 +0530

    tfvars

---
+++ tfvars/variables.tf
@@ -0,0 +1,25 @@
+variable "instances" {
+  type        = map
+}
+
+variable "zone_id" {
+  default = "Z0177980G4CMKCIQPSDN"
+}
+
+variable "domain_name" {
+  default = "doddapaneni.shop"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Terraform = "true"
+    }
+}
+
+variable "tags" {
+    type = map
+}
+
+variable "environment" {
+}
